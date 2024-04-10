c592221de4d85467ac95a595835ca29375c8dbc7
AdamBest01 <82303852+JianzhiGAO1999@users.noreply.github.com>
2024-04-10 14:13:45 +1000

    creat navigation part

---
+++ project/sg-wombat/src/components/navigationbar.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="background">
+        <div class="bigcontainer">
+            <div class="science">
+                Science Gallery
+            </div>
+            <div class="content">
+                <div class="innerbox">
+                    <div class="icon">
+                        tu
+                    </div>
+                    <div class="text">
+                        Dashboard
+                    </div>
+                </div>
+                <div class="innerbox">
+                    <div class="icon">
+                        tu
+                    </div>
+                    <div class="text">
+                        Booking
+                    </div>
+                </div>
+                <div class="innerbox">
+                    <div class="icon">
+                        tu
+                    </div>
+                    <div class="text">
+                        Programs
+                    </div>
+                </div>
+                <div class="innerbox">
+                    <div class="icon">
+                        tu
+                    </div>
+                    <div class="text">
+                        Room Information
+                    </div>
+                </div>
+                <div class="innerbox">
+                    <div class="icon">
+                        tu
+                    </div>
+                    <div class="text">
+                        Schools
+                    </div>
+                </div>
+                <div class="lastbox">
+                    <div class="icon">
+                        tu
+                    </div>
+                    <div class="text">
+                        Export Report 
+                    </div>
+                </div>
+                
+                
+                
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+@import '../assets/navigation_bar.css';
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap');
+</style>
