15812c38d3b9a652bb9e0f52cb51be2510314e1c
evelynuni <127373235+evelynuni@users.noreply.github.com>
2024-04-23 21:11:36 +1000

    update program page display

--- project/sg-wombat/src/components/programPageNew.vue
+++ project/sg-wombat/src/components/programPageNew.vue
@@ -33,7 +33,19 @@
             <!-- main content -->
             <el-main>
                 <el-card>
-                    random txt
+                    <template #header>
+                        <div class="card-header">
+                            <span>Programs</span>
+                            <!-- <el-button>button</el-button> -->
+                            <el-button @click="showAddNewProgramForm">Add New Program</el-button>
+                            <addNewProgram v-model:formVisible="isFormVisible" />
+                            <!-- v-model'deault props is modelValue -->
+                        </div>
+                    </template>
+
+                    <div class="card-content">
+                        <programtable class="listbox" />
+                    </div>
                 </el-card>
 
             </el-main>
@@ -51,6 +63,15 @@
 <script lang="ts" setup>
 import { ArrowDown } from '@element-plus/icons-vue'
 import navigationMenu from './navBarNew.vue';
+import { reactive, ref } from 'vue'
+import programtable from './programtable.vue';
+import addNewProgram from './addNewProgram.vue';
+let isFormVisible = ref(false)
+const showAddNewProgramForm = () => {
+    console.log("Attempting to show form");
+    isFormVisible.value = true;
+    console.log("Form visibility should now be true:", isFormVisible.value);
+}
 
 </script>
 
@@ -123,5 +144,12 @@ import navigationMenu from './navBarNew.vue';
 .main-content {
 
     z-index: 1001;
+}
+
+.card-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
 }
 </style>
