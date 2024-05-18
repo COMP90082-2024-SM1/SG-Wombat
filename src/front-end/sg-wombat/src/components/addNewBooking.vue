3f28fbecb069e27d043935c211e7bd5a7e82bb78
evelynuni <127373235+evelynuni@users.noreply.github.com>
2024-05-19 01:55:18 +1000

    Update addNewBooking.vue

--- src/front-end/sg-wombat/src/components/addNewBooking.vue
+++ src/front-end/sg-wombat/src/components/addNewBooking.vue
@@ -14,7 +14,8 @@
             <!-- step 0 Delivery -->
             <div v-show="currentStep === 0">
                 <el-form-item label="Program Stream">
-                    <el-select v-model="form.programStream" filterable clearable placeholder="Select Program Stream">
+                    <el-select v-model="form.programStream" filterable clearable placeholder="Select Program Stream"
+                        :default-first-option="true">
                         <el-option v-for="option in programStreamOptions" :key="option.value" :label="option.label"
                             :value="option.value"></el-option>
                     </el-select>
@@ -27,7 +28,8 @@
                     </el-radio-group>
                 </el-form-item>
                 <el-form-item label="Status">
-                    <el-select v-model="form.status" placeholder="Select Status">
+                    <el-select v-model="form.status" filterable placeholder="Select Status"
+                        :default-first-option="true">
                         <el-option v-for="option in statusOptions" :key="option.value" :label="option.label"
                             :value="option.value"></el-option>
                     </el-select>
@@ -38,7 +40,7 @@
                 </el-form-item>
                 <el-form-item label="Delivery Location">
                     <el-select v-model="form.deliveryLocation" multiple filterable
-                        placeholder="Select Delivery Location">
+                        placeholder="Select Delivery Location" :default-first-option="true">
                         <el-option v-for="option in deliveryLocationOptions" :key="option.value" :label="option.label"
                             :value="option.value"></el-option>
                     </el-select>
@@ -57,34 +59,61 @@
                     <el-date-picker v-model="form.programDate" type="date"></el-date-picker>
                 </el-form-item>
                 <el-form-item label="Start Time">
-                    <el-time-picker v-model="form.startTime" type="time"></el-time-picker>
+                    <el-time-select v-model="form.startTime" placeholder="Start Time" start="08:30" step="00:05"
+                        end="18:30"></el-time-select>
                 </el-form-item>
+
                 <el-form-item label="End Time">
-                    <el-time-picker v-model="form.endTime" type="time"></el-time-picker>
+                    <el-time-select v-model="form.endTime" placeholder="End Time" start="08:30" step="00:05"
+                        end="18:30"></el-time-select>
                 </el-form-item>
+                <!-- read only computed field-->
                 <el-form-item label="Run Time">
-                    <el-input v-model="form.runTime"></el-input>
+                    <el-input v-model="form.runTime" readonly></el-input>
                 </el-form-item>
-                <el-form-item label="Reporting 3hrs">
-                    <el-input v-model="form.reporting3hrs"></el-input>
+                <el-form-item label="Reporting>3hrs">
+                    <el-radio-group v-model="form.reporting3hrs">
+                        <el-radio label="Y">Yes</el-radio>
+                        <el-radio label="N">No</el-radio>
+                    </el-radio-group>
                 </el-form-item>
                 <el-form-item label="Program Category">
                     <el-input v-model="form.programCat"></el-input>
                 </el-form-item>
                 <el-form-item label="Modules">
-                    <el-input v-model="form.modules"></el-input>
+                    <el-select v-model="form.modules" multiple filterable placeholder="Select Modules"
+                        :default-first-option="true">
+                        <el-option v-for="option in moduleOptions" :key="option.value" :label="option.label"
+                            :value="option.value"></el-option>
+                    </el-select>
                 </el-form-item>
                 <el-form-item label="Exhibition">
-                    <el-input v-model="form.exhibition"></el-input>
+                    <el-select v-model="form.exhibition" filterable allow-create placeholder="Select Exhibition"
+                        :default-first-option="true">
+                        <el-option v-for="option in exhibitionOptions" :key="option.value" :label="option.label"
+                            :value="option.value"></el-option>
+                    </el-select>
                 </el-form-item>
                 <el-form-item label="Bus Required">
-                    <el-input v-model="form.busRequired"></el-input>
+                    <el-radio-group v-model="form.busRequired">
+                        <el-radio label="Y">Yes</el-radio>
+                        <el-radio label="N">No</el-radio>
+                        <el-radio label="NA">NA</el-radio>
+                    </el-radio-group>
                 </el-form-item>
+
                 <el-form-item label="Bus Booked">
-                    <el-input v-model="form.busBooked"></el-input>
+                    <el-radio-group v-model="form.busBooked">
+                        <el-radio label="Y">Yes</el-radio>
+                        <el-radio label="N">No</el-radio>
+                        <el-radio label="NA">NA</el-radio>
+                    </el-radio-group>
                 </el-form-item>
                 <el-form-item label="To-Do List Type">
-                    <el-input v-model="form.todoListType"></el-input>
+                    <el-select v-model="form.todoListType" placeholder="Select To-Do List Type">
+                        <el-option v-for="option in todoListTypeOptions" :key="option.value" :label="option.label"
+                            :value="option.value"></el-option>
+                    </el-select>
                 </el-form-item>
                 <el-form-item label="Notes">
                     <el-input type="textarea" v-model="form.notes"></el-input>
@@ -95,10 +124,23 @@
             <!-- step 1 Cohort -->
             <div v-show="currentStep === 1">
                 <el-form-item label="Partner School">
-                    <el-input v-model="form.partnerSchool"></el-input>
+                    <el-radio-group v-model="form.partnerSchool">
+                        <el-radio label="Y">Yes</el-radio>
+                        <el-radio label="N">No</el-radio>
+                    </el-radio-group>
                 </el-form-item>
                 <el-form-item label="Student Year">
-                    <el-input v-model="form.studentYear"></el-input>
+                    <el-checkbox-group v-model="form.studentYear">
+                        <el-checkbox label="7">7</el-checkbox>
+                        <el-checkbox label="8">8</el-checkbox>
+                        <el-checkbox label="9">9</el-checkbox>
+                        <el-checkbox label="10">10</el-checkbox>
+                        <el-checkbox label="11">11</el-checkbox>
+                        <el-checkbox label="12">12</el-checkbox>
+                        <el-checkbox label="VCE">VCE</el-checkbox>
+                        <el-checkbox label="VCE Vocational Major or VET">VCE Vocational Major or
+                            VET</el-checkbox>
+                    </el-checkbox-group>
                 </el-form-item>
                 <el-form-item label="Registered Students Number">
                     <el-input v-model="form.regStudentsNo" type="number"></el-input>
@@ -107,7 +149,10 @@
                     <el-input v-model="form.attendedStudentsNo" type="number"></el-input>
                 </el-form-item>
                 <el-form-item label="Low SES">
-                    <el-input v-model="form.lowSes"></el-input>
+                    <el-radio-group v-model="form.lowSes">
+                        <el-radio label="Y">Yes</el-radio>
+                        <el-radio label="N">No</el-radio>
+                    </el-radio-group>
                 </el-form-item>
                 <el-form-item label="Accessibility Needs">
                     <el-input type="textarea" v-model="form.accNeeds"></el-input>
@@ -145,13 +190,19 @@
             <!-- step 3 Bus -->
             <div v-show="currentStep === 3">
                 <el-form-item label="Process Status">
-                    <el-input v-model="form.processStatus"></el-input>
+                    <el-radio-group v-model="form.processStatus">
+                        <el-radio label="contact">Contact Teacher</el-radio>
+                        <el-radio label="delivered">Delivered</el-radio>
+                    </el-radio-group>
                 </el-form-item>
                 <el-form-item label="Days Remaining">
-                    <el-input v-model="form.daysRemaining"></el-input>
+                    <el-input v-model="daysRemaining" readonly></el-input>
                 </el-form-item>
                 <el-form-item label="Bus Status">
-                    <el-input v-model="form.busStatus"></el-input>
+                    <el-select v-model="form.busStatus" placeholder="Select Bus Status">
+                        <el-option v-for="option in busStatusOptions" :key="option.value" :label="option.label"
+                            :value="option.value"></el-option>
+                    </el-select>
                 </el-form-item>
                 <el-form-item label="Quote">
                     <el-input v-model="form.quote"></el-input>
@@ -175,16 +226,32 @@
                     <el-input v-model="form.busInvoiceNo"></el-input>
                 </el-form-item>
                 <el-form-item label="Saved Receipt">
-                    <el-input v-model="form.savedReceipt"></el-input>
+                    <el-radio-group v-model="form.savedReceipt">
+                        <el-radio label="Y">Yes</el-radio>
+                        <el-radio label="N">No</el-radio>
+                        <el-radio label="NA">NA</el-radio>
+                    </el-radio-group>
                 </el-form-item>
                 <el-form-item label="Expense Master">
-                    <el-input v-model="form.expenseMaster"></el-input>
+                    <el-radio-group v-model="form.expenseMaster">
+                        <el-radio label="Y">Yes</el-radio>
+                        <el-radio label="N">No</el-radio>
+                        <el-radio label="NA">NA</el-radio>
+                    </el-radio-group>
                 </el-form-item>
                 <el-form-item label="PIN Email">
-                    <el-input v-model="form.pinEmail"></el-input>
+                    <el-radio-group v-model="form.pinEmail">
+                        <el-radio label="Y">Yes</el-radio>
+                        <el-radio label="N">No</el-radio>
+                        <el-radio label="NA">NA</el-radio>
+                    </el-radio-group>
                 </el-form-item>
                 <el-form-item label="Times in MSC">
-                    <el-input v-model="form.timesInMSC"></el-input>
+                    <el-radio-group v-model="form.timesInMSC">
+                        <el-radio label="Y">Yes</el-radio>
+                        <el-radio label="N">No</el-radio>
+                        <el-radio label="NA">NA</el-radio>
+                    </el-radio-group>
                 </el-form-item>
                 <el-form-item label="Bus Notes">
                     <el-input type="textarea" v-model="form.busNotes"></el-input>
@@ -218,7 +285,7 @@
 </template>
 
 <script setup>
-import { ref, reactive, computed } from 'vue';
+import { ref, reactive, computed, watch } from 'vue';
 import { defineProps, defineEmits } from 'vue';
 import { Document, School, Van, User, Memo } from '@element-plus/icons-vue'
 
@@ -249,12 +316,12 @@ const form = ref({
     prefDate1: '2024-06-18',
     prefDate2: '2024-06-20',
     programDate: '2024-06-18',
-    startTime: '',
+    startTime: '09:00',
     endTime: '',
     runTime: '',
     reporting3hrs: '',
     programCat: 'Other Workshops',
-    modules: 'W: Future Food; W: Sustainable Communities',
+    modules: ['W: Future Food', 'W: Sustainable Communities'],
     exhibition: 'Non-Exhbition Linked',
     busRequired: 'N',
     busBooked: 'NA',
@@ -262,7 +329,7 @@ const form = ref({
     notes: 'TBC whether SCoE paying for buses',
     // cohort
     partnerSchool: 'Y',
-    studentYear: '11,12',
+    studentYear: ['11', '12'],
     regStudentsNo: 50,
     attendedStudentsNo: 43,
     lowSes: 'N',
@@ -278,7 +345,8 @@ const form = ref({
     teachingArea: 'Humanities Leader, Geography & Psychology Teacher',
     // bus
     processStatus: '',
-    daysRemaining: '',
+    // no need to send it to database?
+    // daysRemaining: '',
     busStatus: '',
     quote: '',
     priceWoGST: '',
@@ -299,16 +367,6 @@ const form = ref({
 
 })
 
-// const form = reactive({
-//     name: '',
-//     maxPeople: 1,
-//     techRequirement: '',
-//     costPerPerson: 0,
-//     runtime: '',
-//     description: '',
-//     status: 'active',
-//     workDays: []  // Initialize the workDays array for checkboxes
-// });
 
 const onSubmit = () => {
     console.log('Submitted:', form);
@@ -341,6 +399,84 @@ const deliveryLocationOptions = ref([
     { label: 'Location 3', value: 'Location 3' },
     // 
 ])
+
+// todo: modules should come from program list (active + upcoming programs)
+const moduleOptions = ref([
+    { label: 'W: Future Food', value: 'Module 1' },
+    { label: 'W: Sustainable Communities', value: 'Module 2' },
+    { label: 'Module 3', value: 'Module 3' },
+])
+
+const exhibitionOptions = ref([
+    { label: 'Non-Exhbition Linked', value: 'Non-Exhbition Linked' },
+    { label: 'Not Natural', value: 'Not Natural' },
+    { label: 'Ancient Lives', value: 'Ancient Lives' },
+
+])
+
+const todoListTypeOptions = ref([
+    { label: 'Todo List Template1', value: 'Todo List Template1' },
+    { label: 'Todo List Template2', value: 'Todo List Template2' },
+    { label: 'Todo List Template3', value: 'Todo List Template3' },
+])
+
+const busStatusOptions = ref([
+    { label: 'Requested Quote', value: 'quote' },
+    { label: 'Processing', value: 'processing' },
+    { label: 'Paid', value: 'paid' },
+    { label: 'Cancelled', value: 'cancelled' },
+
+])
+
+
+const parseTime = (time) => {
+    const [hours, minutes] = time.split(':').map(Number)
+    return hours * 3600 + minutes * 60
+}
+
+const formatTime = (seconds) => {
+    const h = Math.floor(seconds / 3600).toString().padStart(2, '0')
+    const m = Math.floor((seconds % 3600) / 60).toString().padStart(2, '0')
+    return `${h}:${m}`
+}
+
+// compute run time, is it really needed?
+const runTime = computed(() => {
+    if (form.value.startTime && form.value.endTime) {
+        const start = parseTime(form.value.startTime)
+        const end = parseTime(form.value.endTime)
+        const diff = end - start
+        if (diff >= 0) {
+            return formatTime(diff)
+        } else {
+            return '00:00'
+        }
+    }
+    return '00:00'
+})
+
+// watch for changes in run time and update form value
+watch(runTime, (newVal) => {
+    form.value.runTime = newVal
+})
+
+// compute days remaining
+const daysRemaining = computed(() => {
+    if (!form.value.programDate) {
+        return 0
+    }
+
+    const programDate = new Date(form.value.programDate)
+    const today = new Date()
+
+    programDate.setHours(0, 0, 0, 0)
+    today.setHours(0, 0, 0, 0)
+
+    const diffTime = programDate.getTime() - today.getTime()
+    const diffDays = Math.round(diffTime / (1000 * 60 * 60 * 24))
+
+    return diffDays
+})
 
 </script>
 
